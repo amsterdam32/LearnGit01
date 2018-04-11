2df7dbe39e5ad2589ba7122ea3c657751007a5ba
Vladimir Postman <v.postman2014@yandex.com>
2018-04-11 12:25:32 +0300

    add new rect inside old rect

--- src/com/home/learn/MyPanel.java
+++ src/com/home/learn/MyPanel.java
@@ -14,5 +14,6 @@ public class MyPanel extends JPanel {
         super.paintComponent(g);
         Graphics2D g2 = (Graphics2D)g;
         g2.drawRect(100,100,200,200);
+        g2.drawRect(150,150,100,100);
     }
 }
