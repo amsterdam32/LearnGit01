33a5bbd10c428d7f470421bdd94a84f7aa34107a
Vladimir Postman <v.postman2014@yandex.com>
2018-04-11 11:57:48 +0300

    modifed application for drawing rect

--- src/com/home/learn/MainClass.java
+++ src/com/home/learn/MainClass.java
@@ -1,7 +1,21 @@
 package com.home.learn;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class MainClass {
     public static void main(String[] args) {
-        System.out.println("Learn GitHub opportunities ");
+        EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                JFrame frame = new JFrame("SomeApp");
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame.setVisible(true);
+                MyPanel myPanel = new MyPanel();
+                frame.add(myPanel);
+                frame.pack();
+                frame.setLocationRelativeTo(null);
+            }
+        });
     }
 }
---
+++ src/com/home/learn/MyPanel.java
@@ -0,0 +1,18 @@
+package com.home.learn;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MyPanel extends JPanel {
+
+    MyPanel(){
+        this.setPreferredSize(new Dimension(400,400));
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D)g;
+        g2.drawRect(100,100,200,200);
+    }
+}
