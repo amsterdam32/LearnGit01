1c3b445e064919e87ee7db053b334ac9aa155975
Vladimir Postman <v.postman2014@yandex.com>
2018-04-11 13:57:07 +0300

    add new rect inside circle

--- src/com/home/learn/MyPanel.java
+++ src/com/home/learn/MyPanel.java
@@ -16,5 +16,7 @@ public class MyPanel extends JPanel {
         Graphics2D g2 = (Graphics2D)g;
         g2.drawRect(100,100,200,200);
         g2.drawOval(100,100,200,200);
+        g2.drawRect(150,150,100,100);
+
     }
 }
